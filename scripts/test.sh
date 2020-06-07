51478a09baae241f8f4a330cc5c3d4901675a642
Teddy Wilson <teddy@toeboat.io>
2020-06-07 17:37:41 -0400

    Replace mocha with jest

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Executes defund12.org unit tests
-
-# Root level directory
-GIT_ROOT="$(git rev-parse --show-toplevel)"
-
-# Run Mocha js tests
-"${GIT_ROOT}/node_modules/mocha/bin/mocha"
