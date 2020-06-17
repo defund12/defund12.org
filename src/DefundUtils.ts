3141aabd5f9bef7afda8a79b4e20d0c46009dbe6
Dion Larson <me@dionlarson.com>
2020-06-17 07:09:10 -0400

    Fix email preview (line breaks in signatures and styling for lists) (#1609)
    * Add remark-breaks to support single line breaks -> br
    
    * Ignore log files
    
    * Add styling to lists

--- src/DefundUtils.ts
+++ src/DefundUtils.ts
@@ -1,5 +1,6 @@
 import unified from "unified";
 import markdown from "remark-parse";
+import breaks from "remark-breaks";
 import rehype from "remark-rehype";
 import sanitize from "rehype-sanitize";
 import html from "rehype-stringify";
@@ -16,6 +17,7 @@ export class DefundUtils {
   static markdownToHTML(value: string): string {
     return unified()
       .use(markdown)
+      .use(breaks)
       .use(rehype)
       .use(sanitize)
       .use(html)
