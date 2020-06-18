c84555f86cfcfd3c107d44d413bb22d85ec3bee1
rpalakkal <roshan.palakkal@gmail.com>
2020-06-17 20:41:12 -0400

    added meta image generation during gatsby build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,12 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+const path = require("path")
+const handlebars = require('handlebars');
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage, createRedirect } = actions;
   const emailComponent = require.resolve("./src/components/email/Email.tsx");
+
 
   const emailData = await graphql(`
     {
@@ -10,6 +16,8 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
             frontmatter {
               permalink
               redirect_from
+              city
+              state
             }
           }
         }
@@ -21,13 +29,15 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panicOnBuild("Error while querying for email data.");
     return;
   }
-  emailData.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  emailData.data.allMarkdownRemark.edges.forEach(async ({ node }) => {
     // Create individual pages
     createPage({
       path: node.frontmatter.permalink,
       component: emailComponent,
       context: {
         permalink: node.frontmatter.permalink,
+        city: node.frontmatter.city,
+        state: node.frontmatter.state
       },
     });
     // Create redirects
@@ -41,4 +51,47 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       });
     }
   });
+};
+
+/**
+ * To generate meta previews
+ * Uses puppeteer 
+*/
+
+//initialize global variables
+let browser = null;
+let previewTemplate = null;
+let template;
+
+//at start of build process, launch Puppeteer instance and read the template file
+exports.onPreInit = async () => {
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    previewTemplate = fs.readFileSync(path.resolve(__dirname, "imageGeneration/previewImage.html"), "utf8");
+    template = handlebars.compile(previewTemplate)
+};
+
+//for every node which is an email, generate a preview image
+exports.onCreateNode = async ({ node, actions }) => {
+  if(node.context && node.context.state){ //check to see if it contains state(to see if it is an email)
+    //check if file already exists; only generate if it doesn't
+    fs.access("./public/images/"+node.context.permalink+".png", fs.F_OK, async (err) => { 
+      if (err) {
+        let content={city:node.context.city, state:node.context.state}
+        let html = template(content, { waitUntil: "load" }) 
+        const page = await browser.newPage();
+        await page.setContent(html);
+        await page.evaluateHandle("document.fonts.ready");
+        let element = await page.$('body');
+        await element.screenshot({path : "./public/images/"+node.context.permalink+".png"})
+      } 
+    })
+  } 
+}
+
+//close Puppeteer instance after build
+exports.onPostBuild = async () => {
+  // Close the browser at the end
+  await browser.close();
 };
