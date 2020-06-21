445449a46b51603e5d3abc8214479df344f4a4c2
ceshion <shionu11@gmail.com>
2020-06-21 04:15:17 -0400

    added letters page, adjusted footer link

---
+++ src/pages/letters.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { PageProps } from "gatsby";
+import Layout from "../components/common/Layout";
+import EmailList from "../components/email-list/EmailList";
+import { SiteProps } from "../types/PropTypes";
+
+/**
+ * The landing/home/root page. Defund 12!
+ */
+export default class Index extends React.Component<PageProps<SiteProps>> {
+  /**
+   * Initialize the component.
+   * @param {PageProps<SiteProps>} props
+   */
+  constructor(props: PageProps<SiteProps>) {
+    super(props);
+  }
+
+  /**
+   * React render method.
+   * @return {React.ReactNode} the rendered component
+   */
+  render(): React.ReactNode {
+    return (
+      <Layout>
+        <EmailList layout="letter" />
+      </Layout>
+    );
+  }
+}
